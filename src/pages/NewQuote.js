b6bb8b93134499974b71ab285fd3fc2ae40a61f1
Brian Bycynski <high5bri@hotmail.com>
2021-11-29 03:37:33 -0500

    react-router

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,12 +1,18 @@
 //
 //
 
+import { useHistory } from 'react-router-dom';
+
 import React from 'react';
 import QuoteForm from '../components/quotes/QuoteForm';
 
 const NewQuote = () => {
+  const history = useHistory();
+
   const addQuoteHandler = (quoteDate) => {
     console.log(quoteDate);
+
+    history.push('/quotes');
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
